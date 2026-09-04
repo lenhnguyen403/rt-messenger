337047590ba7e2b6222ac3a28f1dd1e9f529f1aa
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2026-09-04 11:44:38 +0700

    Change database url

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -3,7 +3,7 @@ import { Env } from './env.config'
 
 const connectDatabase = async () => {
     try {
-        await mongoose.connect(Env.MONGO_URL)
+        await mongoose.connect(Env.MONGO_URI)
         console.log('Database connected successfully')
     } catch (error) {
         console.error("Database connection error:", error);
--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -3,7 +3,7 @@ import { getEnv } from "../utils/get-env";
 export const Env = {
     NODE_ENV: getEnv("NODE_ENV", "development"),
     PORT: getEnv("PORT", "8000"),
-    MONGO_URL: getEnv("MONGO_URI"),
+    MONGO_URI: getEnv("MONGO_URI"),
     JWT_SECRET: getEnv("JWT_SECRET", "your-jwt-secret-key"),
     JWT_EXPIRES_IN: getEnv("JWT_EXPIRES_IN", "15m"),
     FRONTEND_ORIGIN: getEnv("FRONTEND_ORIGIN", "http://localhost:5173"),
